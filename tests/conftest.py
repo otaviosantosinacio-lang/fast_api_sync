cd39cf153208e4336b13a9e32c766afdc1519749
Otavio Santos Inacio <otaviosantosinacio@gmail.com>
2026-04-23 19:29:40 -0300

    Iniciando com SQL Alchemy

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
 
 from src.fast_api.app import app
-
+from src.fast_api.models import table_registry
 
 @pytest.fixture
 def client():
     return TestClient(app=app)
+
+@pytest.fixture
+def session():
+      engine = create_engine('sqlite:///:memory:')
+      table_registry.metadata.create_all(engine)
+
+      table_registry.metadata.drop_all(engine)
